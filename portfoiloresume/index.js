c05b2bce3cf095ab749ec8ecd6d292d2503e2882
Bala <167194179+bala-077@users.noreply.github.com>
2024-05-11 14:18:48 +0530

    Add files via upload

---
+++ portfoiloresume/index.js
@@ -0,0 +1,21 @@
+function edu(){
+    document.querySelector("#education").scrollIntoView({
+        behavior: 'smooth'
+    })
+}
+function skill(){
+    document.querySelector("#skills").scrollIntoView({
+        behavior: 'smooth'
+    })
+}
+function home(){
+    document.querySelector("#homepage").scrollIntoView({
+        behavior: 'smooth'
+    })
+}
+function contact(){
+    document.querySelector("#contact-us").scrollIntoView({
+        behavior: 'smooth'
+    })
+}
+ 
